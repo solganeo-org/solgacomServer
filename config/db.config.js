de0d4f67c1488caadb21c48516a2b31fe700716f
Rodrigo Uriel Zuniga Tellez <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2021-08-19 17:02:22 +0200

    Feat: Read env variables

--- config/db.config.js
+++ config/db.config.js
@@ -1,24 +1,19 @@
-'use strict';
-
-const mysql = require('mysql');
+const mysql = require('mysql')
+const dotenv = require('dotenv').config()
 
 //local mysql db connection
 
 const dbConn = mysql.createConnection({
-    
-    host     : 'localhost',
-    user     : 'root',  
-    password : '1234',  
-    database : 'solganeo_web_push'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '1234',
+  database: 'solganeo_web_push',
+})
 
-});
+dbConn.connect(function (err) {
+  if (err) throw err
 
-dbConn.connect(function(err) {
-    
-    if (err) throw err;  
-    
-    console.log("Database Connected!");
+  console.log('Database Connected!')
+})
 
-});
-
-module.exports = dbConn;
+module.exports = dbConn
