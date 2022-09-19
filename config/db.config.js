df9f7ba92796c7b357b4e006537de67ba88e14a2
Nominaru <lyes.r237@gmail.com>
2022-09-19 14:35:48 +0200

    Correction db.config(db pw)

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ const mysql = require('mysql')
 const connection = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
+  password: process.env.DB_PASS || '',
   database: 'solganeo_web_push'
 }
 
